eb7c4807e2302ada061dacb5eb8b3fa861518ace
Kass Badani <username@users.noreply.github.com>
2025-02-12 10:02:44 -0400

    Refactor typing function

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
-const txt = " Hi mochi I'm testing if this works";
-let i = 0;
-const speed = 100;
+const texts = [
+  "I was wandering if you...",
+  "if you want to...",
+  "goo.. if u want of course..",
+  "u want to go with me...to",
+];
 
+const speed = 100;
+const initialText = document.getElementById("initial-text");
 const textElement = document.getElementById("typing");
 
 function xD() {
-  textElement.textContent = "";
+  textElement.remove();
 }
 
-function typeText() {
-  if (i < txt.length + 1) {
-    textElement.textContent += txt.charAt(i);
-    i++;
-    setTimeout(typeText, speed);
+//que asco as recursivas pero me sirven xd
+
+function typeText(txt, textElement, callback) {
+  let i = 0;
+  function typing() {
+    if (i < txt.length) {
+      textElement.textContent += txt.charAt(i);
+      i++;
+      setTimeout(typing, speed);
+    } else {
+      callback();
+    }
   }
+  typing();
+}
+
+function startTyping() {
+  let i = 0;
+
+  function typeNextLine() {
+    if (i < texts.length) {
+      const newDiv = document.createElement("div");
+      initialText.appendChild(newDiv);
+
+      typeText(texts[i], newDiv, () => {
+        i++;
+        typeNextLine();
+      });
+    }
+  }
+
+  typeNextLine();
 }
 
 setTimeout(xD, 100);
-typeText();
+setTimeout(startTyping, 100);
