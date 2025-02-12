cc0b573345cc45ff8bdef98f62a21bb983d8dc9f
Kass Badani <username@users.noreply.github.com>
2025-02-12 00:17:01 -0400

    Add typing simulation

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello mochi!");
+const txt = " Hi mochi I'm testing if this works";
+let i = 0;
+const speed = 100;
+
+const textElement = document.getElementById("typing");
+
+function xD() {
+  textElement.textContent = "";
+}
+
+function typeText() {
+  if (i < txt.length + 1) {
+    textElement.textContent += txt.charAt(i);
+    i++;
+    setTimeout(typeText, speed);
+  }
+}
+
+setTimeout(xD, 100);
+typeText();
